3f380def2546920ba1bc22a97078b61b64c2af00
Daniel Sanche <d.sanche14@gmail.com>
2016-01-24 14:45:49 -0600

    implemented the algorithm to find the best next move

--- FindNextMove.m
+++ FindNextMove.m
@@ -1,8 +1,19 @@
 function [ newPosition ] = FindNextMove( Position, Destination, G, M )
 
+nextNodes = neighbors(G, Position);
+destNode = repmat(Destination, length(nextNodes), 1);
 
-newPosition = Destination;
+Distances = ManhattenDistance(destNode, nextNodes, M);
 
+thisNode = repmat(Position, length(nextNodes), 1);
+Edges = findedge(G,thisNode,nextNodes);
+Weight = G.Edges.Weight(Edges);
+
+TotalCost = Distances + Weight
+
+[~, idx] = min(TotalCost);
+
+newPosition = nextNodes(idx);
 
 end
 
