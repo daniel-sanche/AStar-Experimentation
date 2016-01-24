415d67ca89d8d6082ae1e7489d6704afd44267c4
Daniel Sanche <d.sanche14@gmail.com>
2016-01-23 23:53:13 -0600

    added function for calculating manhattan distances between points

---
+++ ManhattenDistance.m
@@ -0,0 +1,13 @@
+function [ Dist ] = ManhattenDistance( pt1, pt2, M )
+
+n = ceil(sqrt(M));
+row1 = floor(pt1 / n);
+row2 = floor(pt2 / n);
+
+col1 = mod(pt1, n);
+col2 = mod(pt2, n);
+
+Dist = abs(row1 - row2) + abs(col1 - col2)
+
+end
+
