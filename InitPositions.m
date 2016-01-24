4744ce092b89928ff9443ed12e7e616260b430e4
Daniel Sanche <d.sanche14@gmail.com>
2016-01-24 13:10:29 -0600

    now every package start/end position will be unique

--- InitPositions.m
+++ InitPositions.m
@@ -5,17 +5,21 @@ function [ Vehicles, Packages, Garage  ] = InitPositions( G, N, K )
     startPoint = randi([1 nodeCount],1,1);
     Vehicles = struct('position', repmat({startPoint}, N, 1), 'packages', repmat({[]}, N, 1), 'goal', repmat({0}, N, 1));
     %create vehicles
+    
+    ClaimedSpots = [startPoint];
 
     Packages = struct;
     for i=1:K
         rand1 = startPoint;
         rand2 = startPoint;
-        while rand1 == startPoint
+        while ismember(rand1, ClaimedSpots)
             rand1 = randi([1 nodeCount],1,1);
         end
-        while rand2 == startPoint || rand2 == rand1
+        ClaimedSpots = [ClaimedSpots, rand1];
+        while ismember(rand2, ClaimedSpots)
             rand2 = randi([1 nodeCount],1,1);
         end
+        ClaimedSpots = [ClaimedSpots, rand2];
        Packages(i).position =  rand1;
        Packages(i).destination =  rand2;
        Packages(i).claimed =  false;
