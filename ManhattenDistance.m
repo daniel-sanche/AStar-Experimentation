496753101046e30a601f8a48e9f6e120a6f587c4
Daniel Sanche <d.sanche14@gmail.com>
2016-01-23 23:57:14 -0600

    fixed bug

--- ManhattenDistance.m
+++ ManhattenDistance.m
@@ -1,11 +1,11 @@
 function [ Dist ] = ManhattenDistance( pt1, pt2, M )
 
 n = ceil(sqrt(M));
-row1 = floor(pt1 / n);
-row2 = floor(pt2 / n);
+row1 = floor((pt1-1) / n)
+row2 = floor((pt2-1) / n)
 
-col1 = mod(pt1, n);
-col2 = mod(pt2, n);
+col1 = mod((pt1-1), n)
+col2 = mod((pt2-1), n)
 
 Dist = abs(row1 - row2) + abs(col1 - col2)
 
