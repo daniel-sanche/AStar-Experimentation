9568cb16a44887b05377b5048a51ec22b9fd043c
Daniel Sanche <d.sanche14@gmail.com>
2016-01-24 13:47:36 -0600

    made it so that delivered packages won't show up on the map

--- DisplayMap.m
+++ DisplayMap.m
@@ -2,8 +2,11 @@ function [  ] = DisplayMap( G, Vehicles, Packages, Garage )
 
 h = plot(G);
 
-highlight(h,[Packages(:).position],'NodeColor','r', 'Marker', 's', 'MarkerSize', 10)
-highlight(h,[Packages(:).destination],'NodeColor','magenta', 'Marker', 'o', 'MarkerSize', 10)
+UndeliveredIdx = find([Packages.destination] ~= [Packages.position]);
+UndeliveredPackages = Packages(UndeliveredIdx);
+
+highlight(h,[UndeliveredPackages(:).position],'NodeColor','r', 'Marker', 's', 'MarkerSize', 10)
+highlight(h,[UndeliveredPackages(:).destination],'NodeColor','magenta', 'Marker', 'o', 'MarkerSize', 10)
 highlight(h,[Vehicles(:).position],'NodeColor','g', 'Marker', '>', 'MarkerSize', 20)
 highlight(h,Garage,'NodeColor','b', 'Marker', 'o', 'MarkerSize', 15)
 
