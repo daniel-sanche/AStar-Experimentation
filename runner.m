8ff1a00233460fe3807519a3d7912d9bd94832e5
Daniel Sanche <d.sanche14@gmail.com>
2016-01-24 12:54:44 -0600

    implemented dropping and picking up abilities

--- runner.m
+++ runner.m
@@ -6,10 +6,36 @@ P = 2
 G = GridGraphGenerator(M);
 [ Vehicles, Packages, GaragePt  ] = InitPositions(G, N, K);
 DisplayMap(G, Vehicles, Packages, GaragePt);
+
+%initialize initial goals for each vehicle
 for i=1:length(Vehicles)
     Vehicles(i).goal = FindGoal( Vehicles(i), Packages, [Vehicles(:).goal], P, M );
-    %if there is nothing left to pick up, head back to the garage
-    if Vehicles(i).goal == 0 && Vehicles(i).position != GaragePt
-        Vehicles(i).goal = GaragePt
+end
+
+for i=1:length(Vehicles)
+    Vehicles(i).position = Vehicles(i).goal
+
+    %reached goal. Preform action
+    if Vehicles(i).position == Vehicles(i).goal
+        %attempt to pick up package
+        idx = find([Packages.position] == Vehicles(i).position & [Packages.claimed] == 0);
+        if length(idx) ~= 0
+            thisPackage = Packages(idx);
+            Packages(idx).claimed = true;
+            holdingCount = length(Vehicles(i).packages);
+            Vehicles(i).packages(holdingCount+1) = idx;
+        else
+            %%attempt to drop off package
+            dropIdx = find([Packages.destination] == Vehicles(i).position);
+            droppers = setdiff(dropIdx, Vehicles(i).packages);
+            Vehicles(i).packages = setdiff(Vehicles(i).packages, droppers);
+        end
+        %find new goal
+        Vehicles(i).goal = FindGoal( Vehicles(i), Packages, [Vehicles(:).goal], P, M );
+        %if there is nothing left to pick up, head back to the garage
+        if Vehicles(i).goal == 0 && Vehicles(i).position ~= GaragePt
+            Vehicles(i).goal = GaragePt;
+        end
     end
 end
+DisplayMap(G, Vehicles, Packages, GaragePt);
