87e8056d03eac2c01af7bb1c6f861a8bacf5a14c
Daniel Sanche <d.sanche14@gmail.com>
2016-01-24 12:21:25 -0600

    added logic to send the vehicle back to the garage at the end

--- runner.m
+++ runner.m
@@ -4,6 +4,12 @@ K = 2;
 P = 2
 
 G = GridGraphGenerator(M);
-[ Vehicles, Packages, Garage  ] = InitPositions(G, N, K);
-DisplayMap(G, Vehicles, Packages, Garage);
-Vehicles(1).goal = FindGoal( Vehicles(1), Packages, [Vehicles(:).goal], P, M );
+[ Vehicles, Packages, GaragePt  ] = InitPositions(G, N, K);
+DisplayMap(G, Vehicles, Packages, GaragePt);
+for i=1:length(Vehicles)
+    Vehicles(i).goal = FindGoal( Vehicles(i), Packages, [Vehicles(:).goal], P, M );
+    %if there is nothing left to pick up, head back to the garage
+    if Vehicles(i).goal == 0 && Vehicles(i).position != GaragePt
+        Vehicles(i).goal = GaragePt
+    end
+end
