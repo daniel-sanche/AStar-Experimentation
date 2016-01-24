b75b82eeb8a6d901d5f56806c166310e8d8a0f52
Daniel Sanche <d.sanche14@gmail.com>
2016-01-24 11:22:27 -0600

    changed vehicle and package arrays into structs

--- DisplayMap.m
+++ DisplayMap.m
@@ -2,9 +2,9 @@ function [  ] = DisplayMap( G, Vehicles, Packages, Garage )
 
 h = plot(G);
 
-highlight(h,Packages(:,1),'NodeColor','r', 'Marker', 's', 'MarkerSize', 10)
-highlight(h,Packages(:,2),'NodeColor','magenta', 'Marker', 'o', 'MarkerSize', 10)
-highlight(h,Vehicles,'NodeColor','g', 'Marker', '>', 'MarkerSize', 20)
+highlight(h,[Packages(:).startPt],'NodeColor','r', 'Marker', 's', 'MarkerSize', 10)
+highlight(h,[Packages(:).endPt],'NodeColor','magenta', 'Marker', 'o', 'MarkerSize', 10)
+highlight(h,[Vehicles(:).position],'NodeColor','g', 'Marker', '>', 'MarkerSize', 20)
 highlight(h,Garage,'NodeColor','b', 'Marker', 'o', 'MarkerSize', 15)
 
 end
--- InitPositions.m
+++ InitPositions.m
@@ -3,10 +3,10 @@ function [ Vehicles, Packages, Garage  ] = InitPositions( G, N, K )
     nodeCount = numnodes(G);
 
     startPoint = randi([1 nodeCount],1,1);
-    Vehicles = repmat(startPoint,N,1);
+    Vehicles = struct('position', repmat({startPoint}, N, 1));
+    %create vehicles
 
-    Packages = zeros(K, 2);
-    
+    Packages = struct;
     for i=1:K
         rand1 = startPoint;
         rand2 = startPoint;
@@ -16,8 +16,8 @@ function [ Vehicles, Packages, Garage  ] = InitPositions( G, N, K )
         while rand2 == startPoint || rand2 == rand1
             rand2 = randi([1 nodeCount],1,1);
         end
-       Packages(i,1) =  rand1;
-       Packages(i,2) =  rand2;
+       Packages(i).startPt =  rand1;
+       Packages(i).endPt =  rand2;
     end
 
     Garage = startPoint;
