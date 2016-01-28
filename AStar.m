906805b2717d8bb453016c87bd4a0d6dc5632417
Daniel Sanche <d.sanche14@gmail.com>
2016-01-27 22:04:05 -0600

    made it so that the vehicles can successively pick up packages

--- AStar.m
+++ AStar.m
@@ -10,58 +10,68 @@ function [ path, cost ] = AStar( Vehicles, Packages, Garage, G, M, P )
     %add new options to the queue
 
     PriorityQueue = [0, {Vehicles}, {repmat({[]}, 1, numVehicles)}, {[Packages.position]}];
-    while ~reachedGoal(Vehicles, Packages, Garage)
+    done = false;
+    while ~done
         %take the first choice off the queue
         VehiclePositions = cell2mat(PriorityQueue(1,2));
         PackagesCarried = PriorityQueue{1,3};
         PackagePositions = PriorityQueue{1,4};
         PriorityQueue(1,:) = [];
         
-        CombinedOptions = zeros(maxChoices ^ numVehicles, numVehicles);
-        CombinedCarrying = cell(maxChoices ^ numVehicles, numVehicles);
-        for i=1:numVehicles
-            
-           [newChoices, newCarryingList] = generateNewChoices(VehiclePositions(i), PackagesCarried{i}, PackagePositions, G, maxChoices, P);
-          
-           eachNumSize = maxChoices^(numVehicles-i);
-           numBlocks = maxChoices ^ (i-1);
-
-           startPos = 1;
-           for k=1:numBlocks
-                for j=1:maxChoices
-                    %fill the block's space with the numbers repeated as
-                    %many times as will fit
-                    CombinedOptions(startPos:startPos+eachNumSize-1,i) = newChoices(j);
-                    CombinedCarrying(startPos:startPos+eachNumSize-1,i) = newCarryingList(i);
-                    startPos = startPos+eachNumSize;
-                end
-           end
-        end
-        %remove all rows with a 0
-        RowsWithZeros = all(CombinedOptions,2);
-        CombinedOptions = CombinedOptions(RowsWithZeros,:);
-        CombinedCarrying = CombinedCarrying(RowsWithZeros,:);
-        Values = HeuristicValues(CombinedOptions, Packages, Garage, M);
-        %Values = Values + CostToHere + 1;
-
-        %add to priority queue
-        currentLength = size(PriorityQueue,1);
-        newLength = currentLength + size(Values,1);
-        PriorityQueue(currentLength+1:newLength,1) = num2cell(Values);
-        PriorityQueue(currentLength+1:newLength,2) = num2cell(CombinedOptions, 2);
-        PriorityQueue(currentLength+1:newLength,3) = num2cell(CombinedCarrying, 2);
-        %sort the new queue
-        [~, Index_A] = sort(cell2mat(PriorityQueue(:,1)));
-        PriorityQueue = PriorityQueue(Index_A,:);
+        % check to see if we've reached our goal
+        done = reachedGoal(VehiclePositions, PackagePositions, [Packages.destination], Garage);
+        if ~done
         
+            CombinedOptions = zeros(maxChoices ^ numVehicles, numVehicles);
+            CombinedCarrying = cell(maxChoices ^ numVehicles, numVehicles);
+            for i=1:numVehicles
+
+               [newChoices, newCarryingList] = generateNewChoices(VehiclePositions(i), PackagesCarried{i}, PackagePositions, G, maxChoices, P);
+
+               eachNumSize = maxChoices^(numVehicles-i);
+               numBlocks = maxChoices ^ (i-1);
+
+               startPos = 1;
+               for k=1:numBlocks
+                    for j=1:maxChoices
+                        %fill the block's space with the numbers repeated as
+                        %many times as will fit
+                        CombinedOptions(startPos:startPos+eachNumSize-1,i) = newChoices(j);
+                        CombinedCarrying(startPos:startPos+eachNumSize-1,i) = newCarryingList(j);
+                        startPos = startPos+eachNumSize;
+                    end
+               end
+            end
+            %remove all rows with a 0
+            RowsWithZeros = all(CombinedOptions,2);
+            CombinedOptions = CombinedOptions(RowsWithZeros,:);
+            CombinedCarrying = CombinedCarrying(RowsWithZeros,:);
+
+            %update package positions
+            NewPackagePositions = UpdatePackagePositions(CombinedOptions, CombinedCarrying, PackagePositions);
+
+            Values = HeuristicValues(CombinedOptions, NewPackagePositions, CombinedCarrying, [Packages.destination], Garage, M);
+            %Values = Values + CostToHere + 1;
+
+            %add to priority queue
+            currentLength = size(PriorityQueue,1);
+            newLength = currentLength + size(Values,1);
+            PriorityQueue(currentLength+1:newLength,1) = num2cell(Values);
+            PriorityQueue(currentLength+1:newLength,2) = num2cell(CombinedOptions, 2);
+            PriorityQueue(currentLength+1:newLength,3) = num2cell(CombinedCarrying, 2);
+            PriorityQueue(currentLength+1:newLength,4) = num2cell(NewPackagePositions, 2);
+            %sort the new queue
+            [~, Index_A] = sort(cell2mat(PriorityQueue(:,1)));
+            PriorityQueue = PriorityQueue(Index_A,:);
+        end
     end
 
 end
 
-function [done] = reachedGoal(Vehicles, Packages, Garage)
+function [done] = reachedGoal(Vehicles, PackagesPos, PackageDest, Garage)
     done = false;
     if max(Vehicles - Garage) == 0 
-        if max([Packages.position] - [Packages.destination]) == 0
+        if max(PackagesPos - PackageDest) == 0
             done = true; 
         end
     end
@@ -76,7 +86,7 @@ function [newChoices, newCarry] = generateNewChoices(CurrentPosition, PackagesCa
     
     %add option to pick up package if you're on one
     idxOfPackageAtCurrentPos = find(PackagePositions==CurrentPosition);
-    if(~isempty(idxOfPackageAtCurrentPos) && isempty(find(PackagesCarried==idxOfPackageAtCurrentPos)) && numel(PackagesCarried < P))
+    if(~isempty(idxOfPackageAtCurrentPos) && isempty(find(PackagesCarried==idxOfPackageAtCurrentPos)) && numel(PackagesCarried) < P)
         newChoices(6) = CurrentPosition;
         newCarry{6} = [PackagesCarried, idxOfPackageAtCurrentPos];
     end
@@ -88,24 +98,43 @@ function [newChoices, newCarry] = generateNewChoices(CurrentPosition, PackagesCa
     end    
 end
 
-function [values] = HeuristicValues(newPositionsArray, Pacakges, Garage, M)
+function [values] = HeuristicValues(newPositionsArray, packagePositions, PackagesCarried, PackageDests, Garage, M)
     %create a 3d array. Each 3rd D stack represents the distance to a
     %certain package. Each row represents a different decision option. Each column
     %represents a different robot. For each decision, we want to find the
     %sum of min distances from any robot to each package
     [rows, cols] = size(newPositionsArray);
-    packagePositions = reshape([Pacakges.position],[1,1,length([Pacakges.position])]);
-    packagePositions = repmat(packagePositions, rows,cols,1);
-    vehiclePositions = repmat(newPositionsArray, 1,1,length(Pacakges));
+    numPackages = size(packagePositions,2);
+    reshapedPackages = reshape(packagePositions,[rows,1,numPackages])
+    reshapedPackages = repmat(reshapedPackages, 1,cols,1)
     
-    distances = ManhattenDistance(vehiclePositions, packagePositions, M);
+    vehiclePositions = repmat(newPositionsArray, 1,1,numPackages);
+    
+    distances = ManhattenDistance(vehiclePositions, reshapedPackages, M);
     
     %this tells us the minimum distance to each package (3rd d) for each
     %potential move (1st D)
     minDists = min(distances, [], 2);
     %we add the two together to result in the total
     sumDist = sum(minDists, 3);
-    values = sumDist;
+    
+    %we also want to add a cost for every package not being carried
+    destinationsFormated = repmat(PackageDests, rows, 1);
+    posDiff = packagePositions - destinationsFormated
+    numDelivered = sum((posDiff == 0),2);
+    [~, numCarried] = cellfun(@size, PackagesCarried);
+    numCarried = sum(numCarried,2);
+    totalPackages = repmat(numPackages, rows, 1);
+    numNotPickedUp = totalPackages - numCarried - numDelivered;
+    
+    
+    
+    values = sumDist + numNotPickedUp;
+end
+
+function [NewPositions] = UpdatePackagePositions(CombinedOptions, CombinedCarrying, OldPositions)
+    numberOptions = size(CombinedOptions, 1);
+    NewPositions = repmat(OldPositions, numberOptions, 1);
 end
 
 
