80e6b18f0c09d5ac43c08e0e4d4e81797b9991b9
Daniel Sanche <d.sanche14@gmail.com>
2016-02-06 21:38:15 -0600

    changed runner script into main function

---
+++ Main.m
@@ -0,0 +1,20 @@
+function Main(M, N, K, P)
+    M = 15;
+    N = 2;
+    K = 2;
+    P = 1;
+
+    javaaddpath('./javatuples-1.2.jar');
+
+    if(M < N*2 + 1)
+       error('not enough locations for that many packages (M < N*2 + 1)'); 
+    end
+
+    G = GridGraphGenerator(M, [1 20], 0);
+    [ Vehicles, Packages, GaragePt  ] = InitPositions(G, N, K);
+
+    [Path, Cost] = AStar( N, Packages, GaragePt, G, M, P )
+
+    output = ['finished in ', num2str(Cost),' turns' ];
+    disp(output);
+end
