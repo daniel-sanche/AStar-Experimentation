5d874eee54e88b4bf0eb18fe49bcdee06ab03386
Daniel Sanche <d.sanche14@gmail.com>
2016-02-07 16:22:06 -0600

    now works over command line

--- Main.m
+++ Main.m
@@ -1,4 +1,17 @@
 function Main(M, N, K, P)
+    %convert inputs to number values
+    if ischar(M)
+        M = str2num(M);
+    end
+    if ischar(N)
+        N = str2num(N);
+    end
+    if ischar(K)
+        K = str2num(K);
+    end
+    if ischar(P)
+        P = str2num(P);
+    end
 
     if(M < N*2 + 1)
        error('not enough locations for that many packages (M < N*2 + 1)'); 
